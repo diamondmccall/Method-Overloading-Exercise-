32fe5d6084569ad75d453aef11e38ae668dafa7a
Diamond Mccall <singlikediamond@gmail.com>
2023-01-26 21:28:54 -0600

    initla commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,54 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static int Add(int a, int b)
         {
-            Console.WriteLine("Hello, World!");
+            return a + b;
         }
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isCheck)
+           
+        {
+            var sum = a + b;
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if(isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
+
+        
+
+        
+
+        static void Main(string[] args)
+
+        {
+
+
+            Console.WriteLine(Add(3, 5));
+            Console.WriteLine(Add(.3, .4));
+            Console.WriteLine(Add(6, 7, true));
+
+        
+          
+        }
+        
     }
 }
